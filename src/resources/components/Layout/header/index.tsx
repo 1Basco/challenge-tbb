f047192ee3043d2f54350ffdc5592491cbc819fb
1Basco <lsos13@gmail.com>
2023-07-19 17:55:40 -0300

    [Feature - Header](Added a Basic Header to the application)

--- src/resources/components/Layout/header/index.tsx
+++ src/resources/components/Layout/header/index.tsx
@@ -4,10 +4,26 @@ import tbbLogo from "@/resources/assets/images/tbb-logo.png";
 
 const Header = (): JSX.Element => {
   return (
-    <div className="flex justify-start items-center py-5 px-4 h-16 md:px-24 bg-slate-800 text-white">
-      <Link className="font-semibold max-w-[105px]" to={RouteConstants.ROOT}>
+    <div className="flex items-center py-5 px-4 h-16 md:px-24 bg-slate-800 text-white">
+      <Link
+        className="justify-start font-semibold max-w-[60px] md:max-w-[105px]"
+        to={RouteConstants.ROOT}
+      >
         <img src={tbbLogo} alt="The Brooklyn Brothers Logo" />
       </Link>
+
+      <nav className="mx-auto pr-[60px] md:pr-[105px]">
+        <ul className="flex space-x-8">
+          <li>
+            <Link
+              className="font-semibold text-base md:text-2xl"
+              to={RouteConstants.ROOT}
+            >
+              Challenge TBB
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
